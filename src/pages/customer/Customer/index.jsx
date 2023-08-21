fb7f2a73a17039c5afd552538dec4a898b1fc775
keru01 <tontrongnghia2k1@gmail.com>
2023-08-21 14:46:59 +0700

    Add notification order will overdue after then

--- src/pages/customer/Customer/index.jsx
+++ src/pages/customer/Customer/index.jsx
@@ -8,29 +8,42 @@ import { useMount } from 'ahooks';
 import checkOverDue from '../../../services/Customer/checkOverDue';
 import { useState } from 'react';
 import OverDueModal from './components/OverDueModal';
+import checkWarningOverDue from './../../../services/Customer/checkWarningOverDue';
+import WarningOverDue from './components/WarningOverDue';
 
 const index = () => {
   const [isOpenOverDue, setIsOpenOverDue] = useState(false);
+  const [isOpenWanring, setIsOpenWanring] = useState(false);
   useMount(() => {
     checkOverDue()
-        .then((res) => {
-          if(res.status == 200){
-            setIsOpenOverDue(true)
-          }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-  return( <>
+      .then((res) => {
+        if (res.status == 200) {
+          setIsOpenOverDue(true)
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    checkWarningOverDue()
+      .then((res) => {
+        if (res.status == 200) {
+          setIsOpenWanring(true)
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  return (<>
     <CustomerHeader02 />
     <div>
       <OverDueModal isOpenOverDue={isOpenOverDue} setIsOpenOverDue={setIsOpenOverDue} />
+      <WarningOverDue isOpenWanring={isOpenWanring} setIsOpenWanring={setIsOpenWanring} />
       <Outlet />
     </div>
 
-      <CustomerFooter />
-    </>
+    <CustomerFooter />
+  </>
   );
 };
 
